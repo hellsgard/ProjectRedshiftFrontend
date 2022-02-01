0b8dc6e55cc6ebfd9e1aabe83e568dc417f9a27f
Katie Diggory <Katie.diggory@gmail.com>
2022-02-01 10:47:09 +0000

    Adding the nav bar

--- redshift/src/App.js
+++ redshift/src/App.js
@@ -3,9 +3,11 @@ import './CSS/App.css';
 import Nav from './components/Nav.jsx';
 import Login from './components/Login.jsx';
 import Logout from './components/Logout.jsx';
-import Scenario1 from './components/Scenario1';
-import Scenario2 from './components/Scenario2';
-import Scenario3 from './components/Scenario3';
+import Lock from './components/Lock.jsx';
+import Scenario1 from './components/Scenario1.jsx';
+import Scenario2 from './components/Scenario2.jsx';
+import Scenario3 from './components/Scenario3.jsx';
+import Welcome from './components/Welcome.jsx';
 
 import {
   BrowserRouter,
@@ -29,6 +31,15 @@ function App() {
         <button type="submit" >Login</button>
         <button >Reset</button>
         </form>
+        <BrowserRouter>
+        <Route path='/' element = {<Login/>}></Route>
+        <Route path='/logout' element = {<Logout/>}></Route>
+        <Route path='/lock' element = {<Lock/>}></Route>
+        <Route path='/scenario1' element = {<Scenario1/>}></Route>
+        <Route path='/scenario2' element = {<Scenario2/>}></Route>
+        <Route path='/scenario3' element = {<Scenario3/>}></Route>
+        <Route path='/welcome' element = {<Welcome/>}></Route>
+        </BrowserRouter>
 
     </div>
   );
---
+++ redshift/src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+    return ( 
+        <div>
+            <h2> This is the nav bar </h2>
+            <Link to="/Welcome">
+                <h4> Home </h4>
+                </Link>
+            <Link to="/Lock">
+                <h4> Lock console </h4>
+                </Link>
+                <Link to="/Logout">
+                <h4> Logout </h4>
+                </Link>
+        </div>
+     );
+}
+ 
+export default Nav;
