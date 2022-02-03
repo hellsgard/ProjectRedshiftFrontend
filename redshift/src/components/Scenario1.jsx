e2bf5b68cad9bae57e2916c02c46d1987f3aada5
chrisjones31419 <chrisleonjones@gmail.com>
2022-02-03 14:16:38 +0000

    linking to scn1

--- redshift/src/components/Scenario1.jsx
+++ redshift/src/components/Scenario1.jsx
@@ -1,8 +1,10 @@
 import Nav from "./Nav.jsx";
+import {useParams} from "react-router-dom";
 
 const Scenario1 = () => {
 
 
+  let{citizenID} = useParams();
 
   return (
     <div>
@@ -10,6 +12,7 @@ const Scenario1 = () => {
       <h1> This is the page for scenario 1 </h1>
       <body>
         <h2>HTML Iframes for information</h2>
+        <h2>{citizenID}</h2>
 
         <iframe
           src=""
