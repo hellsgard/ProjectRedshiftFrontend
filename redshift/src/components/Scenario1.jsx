b45000959f13f6103c5415f25bbcca35ef2c5723
hellsgard <85687000+hellsgard@users.noreply.github.com>
2022-02-07 13:43:31 +0000

    Update Scenario1.jsx

--- redshift/src/components/Scenario1.jsx
+++ redshift/src/components/Scenario1.jsx
@@ -1,7 +1,6 @@
 
 import Navb from "./Navb.jsx";
 import "../CSS/Scenario1.css";
-import Navb from "./Nav.jsx";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { selectOptions } from "@testing-library/user-event/dist/select-options";
 import Suspect from './Suspect';
@@ -24,25 +23,12 @@ import { ReactPropTypes } from "react";
 
 const Scenario1 = () => {
 
-  const center = [51.505, -0.09];
-  const rectangle = [
-    [51.49, -0.08],
-    [51.5, -0.06],
-  ];
-
-  const fillBlueOptions = { fillColor: "blue" };
-  const fillRedOptions = { fillColor: "red" };
-  const greenOptions = { color: "green", fillColor: "green" };
-  const purpleOptions = { color: "purple" };
-
-
-  //const BasicData = ({forename, surname, address, dob, sex, passportNumber, nationality, placeOfBirth}) => {
-
   const [suspect, setSuspect] = useState("");
   const [suspectF, setSuspectF] = useState("");
   const [pageLoaded, setPageLoaded] = useState(false);
 
   const { id } = useParams();
+
   useEffect( () => {
     axios.get(`http://localhost:8080/queryPerson/byID/?citizenID=${id}`)
       .then((response) => {
@@ -56,6 +42,7 @@ const Scenario1 = () => {
         console.log(error);
       });
   }, [id]);
+
   console.log(suspect.forenames);
   console.log("THIS THIS THIS")
   console.log(suspect);
@@ -64,26 +51,7 @@ const Scenario1 = () => {
 
   // let forenames= suspect.forenames;
   // let surname= suspect.surname;
-  let address= suspect.homeAddress;
-
-  //trying to get financial data
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:8080/queryPerson/financialEpos/?citizenID=${id}`)
-  //     .then((response) => {
-  //       console.log("hello")
-  //       console.log(response);
-  //       setSuspectF(response.data);
-  //       setPageLoaded(true);
-  //       console.log(response);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, [id]);
-  // console.log(suspectF.accountNumber);
-
-
+ 
   let forenames = suspect.forenames;
   let surname = suspect.surname;
   let address = suspect.homeAddress;
@@ -92,89 +60,16 @@ const Scenario1 = () => {
   let passportNumber= suspect.passportNumber;
   let nationality= suspect.nationality;
   let placeOfBirth= suspect.placeOfBirth;
-  let workplace = suspect.businessName;
-
-
-
-
-
-
-
-
-  // const getMobileData = (() => {
-  //   console.log(suspect.forenames);
-  //     axios.get(`http://localhost:8080/queryPerson/mobile`, {params: mobileQuery})
-  //     .then((response) => {
-  //       console.log(forenames);
-  //       console.log(response); 
-  //   })
-
-
-
-  
-  // return (
-  //   <div>
-
-  //     <Nav></Nav>
-  //     <h4> Scenario 1 </h4>
-  //     <body>
-  //       <div class="container">
-  //         <div class="row align-items-center my-5">
-  //           <div class="col-lg-5">
-  //             <h1 class="font-weight-light">Basic Information</h1>
-
-  //               <p>Full Name: {forenames} {surname}</p>
-  //               <p>Address: {address}</p>
-  //               <p>Date Of Birth: {dob}</p>
-  //               <p>Gender: {gender}</p>
-  //               <p>Passport Number: {passportNumber} </p>
-  //               <p>Nationality: {nationality} </p>
-  //               <p>Place Of Birth: {placeOfBirth}</p>
-                
-  //             <h1 class="font-weight-light">Known Associates</h1>
-  //             <p>
-  //               Lorem Ipsum
-  //             </p>
-  //             <h1 class="font-weight-light">Financial</h1>
-  //             <p>
-  //               Lorem Ipsum
-  //             </p>
-  //             <h1 class="font-weight-light">Past Whereabouts</h1>
-  //             <p>
-  //               Lorem Ipsum
-  //             </p>
-  //             <h1 class="font-weight-light">Call Records</h1>
-        
-  //             <MobileDataInfo forenames={suspect.forenames} surname={suspect.surname} dateOfBirth={suspect.dateOfBirth}/>
-  //             <p>
-  //               Lorem Ipsum
-  //             </p>
-              
-  //             <Map></Map>
-  //             {/* {suspect.map((suspect) => {  
-  //               return(
-                     
-  //                 <Suspect citizenID={suspect.citizenID} forenames={suspect.forenames} surname={suspect.surname} homeAddress={suspect.homeAddress} dateOfBirth={suspect.dateOfBirth} placeOfBirth={suspect.placeOfBirth} sex={suspect.sex}/>     
-                        
-  //           )})}; */}
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <iframe src="" title="Iframe"></iframe>
-  //     </body>
-
-
-
-
+  // let workplace = suspect.businessName;
 
   return (
-    <div>
+      <div>
 
-        <div>
-          <Navb/>
-        </div>
-            <br>
-            </br>
+              <div>
+                <Navb/>
+              </div>
+                <br>
+                </br>
         <h1 class="font-weight-light"> {suspect.forenames} {suspect.surname} </h1>
         <div>
           <Tab.Container id="left-tabs-example" defaultActiveKey="first">
@@ -201,57 +96,13 @@ const Scenario1 = () => {
               <Col sm={9}>
                 <Tab.Content>
                   <Tab.Pane eventKey="first">
-                  <p>Full Name: {suspect.forenames} {suspect.surname}</p>
-
-      <div>
-        <Navb />
-      </div>
-      <br></br>
-      <h1 class="font-weight-light">
-        {" "}
-        Name: {forenames} {surname}{" "}
-      </h1>
-      <div>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-          <Row>
-            <Col sm={2}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Profile information</Nav.Link>
-                  {/* <p>Full Name: {forenames} {surname}</p>
-                  <p>Address: {address}</p>
-                  <p>Date Of Birth: {dob}</p>
-                  <p>Gender: {gender}</p>
-                  <p>Passport Number: {passportNumber} </p>
-                  <p>Nationality: {nationality} </p>
-                  <p>Place Of Birth: {placeOfBirth}</p> */}
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Known associates</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Financial information</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="fourth">Call records</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9}>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <p>
-                    Full Name: {forenames} {surname}
-                  </p>
-
-                  <p>Address: {address}</p>
-                  <p>Date Of Birth: {dob}</p>
-                  <p>Gender: {gender}</p>
-                  <p>Passport Number: {passportNumber} </p>
-                  <p>Nationality: {nationality} </p>
-                  <p>Place Of Birth: {placeOfBirth}</p>
-
-
+                    <p>Full Name: {suspect.forenames} {suspect.surname}</p>
+                    <p>Address: {address}</p>
+                    <p>Date Of Birth: {dob}</p>
+                    <p>Gender: {gender}</p>
+                    <p>Passport Number: {passportNumber} </p>
+                    <p>Nationality: {nationality} </p>
+                    <p>Place Of Birth: {placeOfBirth}</p>
                   </Tab.Pane>
                   <Tab.Pane eventKey="second" title="Associates">
                     {/* {assocData.map((assocData))} */}
@@ -259,82 +110,36 @@ const Scenario1 = () => {
                     <p> </p>
                   </Tab.Pane>
                   <Tab.Pane eventKey="third">
-                    <p></p>
+                    <p>third</p>
                   </Tab.Pane>
                   <Tab.Pane eventKey="fourth">
                     <p></p>
                   </Tab.Pane>
+                  <Tab.Pane eventKey="fifth">
+                    <p></p>
+                  </Tab.Pane>
+
                 </Tab.Content>
               </Col>
 
             </Row>
           </Tab.Container>
-          <Col sm={9}>
-          <h2>fd</h2>
-          </Col>
+
         </div>
 
   
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
           <Map/>
         </MapContainer>
 
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum." Section 1.10.32 of "de Finibus
-                  Bonorum et Malorum", written by Cicero in 45 BC "Sed ut
-                  perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                  consequuntur magni dolores eos qui ratione voluptatem sequi
-                  nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-                  dolor sit amet, consectetur, adipisci velit, sed quia non
-                  numquam eius modi tempora incidunt ut labore et dolore magnam
-                  aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-                  nostrum exercitationem ullam corporis suscipit laboriosam,
-                  nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
-                  iure reprehenderit qui in ea voluptate velit esse quam nihil
-                  molestiae consequatur, vel illum qui dolorem eum fugiat quo
-                  voluptas nulla pariatur?"
-                </Tab.Pane>
-                <Tab.Pane eventKey="Third">
-                  {/* <h5> Bank Name: {bank} </h5>
-                  <h5> Bank Account Number: {accountNumber} </h5>
-                  <h5> Vendor: {vendor} </h5>
-                  <h5> Amount Paid: {amount} </h5>
-                  <h5> Time of Transaction: {timestamp} </h5> */}
-                </Tab.Pane>
-                <Tab.Pane eventKey="fourth">
-                <MobileDataInfo forenames={suspect.forenames} surname={suspect.surname} dateOfBirth={suspect.dateOfBirth}/>
-      
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-        <Col sm={9}>
-          <h2>Map Data</h2>
-        </Col>
-      </div>
-
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-        <Map />
-      </MapContainer>
     </div>
   );
 };
 
+
+export default Scenario1;
+
     
-  
     // const center = [51.505, -0.09];
   // const rectangle = [
   //   [51.49, -0.08],
@@ -345,6 +150,3 @@ const Scenario1 = () => {
   // const fillRedOptions = { fillColor: "red" };
   // const greenOptions = { color: "green", fillColor: "green" };
   // const purpleOptions = { color: "purple" };
-
-
-export default Scenario1;
