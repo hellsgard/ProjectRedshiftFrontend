e73540871b659078064d43bd35851e8232fd2b94
Humairaa Saleh <humairaasaleh@gmail.com>
2022-02-08 14:40:43 +0000

    anpr data

--- redshift/src/components/Vehicles.jsx
+++ redshift/src/components/Vehicles.jsx
@@ -1,32 +1,56 @@
 import axios from 'axios';
 import { useEffect, useState } from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 const Vehicles = () => {
-    const [vehiclesData, setVehiclesData] = useState([]);
+    const [anprData, setData] = useState([]);
     const [error, setError] = useState(null);
     const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        console.log("function running");
-        axios.get("http://localhost:8080/vehicles/readAll")
-            .then((response) => {
-                console.log(response);  // need to destructure data
-                setVehiclesData(response.data);
-                console.log(vehiclesData);
-                setLoaded(true);
-                console.log(response);
-            }).catch((error) => {
-                setLoaded(true);
-                setError(error);
-            })},[]);
-
     return (
         <div>
-            {/* this could be a react fragment - better than doing lots of divs */}
-            <h2> vehicles page </h2>
+            <h4>Vehicle Registration: {anprData.vehicleRegistrationNumber}</h4>
+            <Row>
+                  <Col>
+                    <h3>Vehicle Data</h3>
+                    {anprData.map((data) => {
+                      return (
+                        <div>
+                          <Card style={{ width: '30rem' }}>
+                            <p>Drivers License: {data.driverLicenceID}</p>
+                            <p>Make: {data.make}</p>
+                            <p>Model: {data.model}</p>
+                            <p>Colour: {data.colour}</p>
+                          </Card>
+                        </div>
+                      )
+                    })}
+                  </Col>
+
+                  <Col>
+                    <h3>ANPR Data</h3>
+                    {anprData.map((data) => {
+                      return (
+                        <div>
+                          <Card style={{ width: '30rem' }}>
+                            <p>Street Name: {data.streetName}</p>
+                            <p>Latitude: {data.latitude}</p>
+                            <p>Longitude: {data.longitude}</p>
+                            <p>Timestamp: {data.timestamp}</p>
+                          </Card>
+                        </div>
+                      )
+                    })}
+                  </Col>
+
+                </Row>
+
+
         </div>
 
-    );
+    )
 }
 
 export default Vehicles;
