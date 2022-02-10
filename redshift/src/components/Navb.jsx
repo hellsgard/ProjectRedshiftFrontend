f7fcece620e21ac8c1d85f15c0751b3cf8c6181d
Dan Robinson <danielarobinson2@gmail.com>
2022-02-10 18:22:14 +0000

    navbar fix

--- redshift/src/components/Navb.jsx
+++ redshift/src/components/Navb.jsx
@@ -1,21 +1,22 @@
-import {Link} from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const Navb = () => {
-    return ( 
-        <div>
-            <Navbar variant="dark" bg="dark">
-              {/* <Container> */}
-                  <img src="../RedShift.png" alt="Redshift logo" height="80"></img>
-                <Navbar.Brand href="/home">Home</Navbar.Brand>
-                {/* <Navbar.Brand href="/lock">Lock</Navbar.Brand> */}
-                <Navbar.Brand href="/logout">Logout</Navbar.Brand>
-              {/* </Container> */}
-            </Navbar>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Navbar variant="dark" bg="dark">
+        {/* <Container> */}
+        <img src="../RedShift.png" alt="Redshift logo" height="50"></img>
+        <div className="home p-4"></div>
+        <Navbar.Brand href="/home">Home</Navbar.Brand>
+        {/* <Navbar.Brand href="/lock">Lock</Navbar.Brand> */}
+        <Navbar.Brand href="/logout">Logout</Navbar.Brand>
+        {/* </Container> */}
+      </Navbar>
+    </div>
+  );
+};
+
 export default Navb;
