26abd6a35f793e94c04998b67b2803df48a948dd
Dan Robinson <danielarobinson2@gmail.com>
2022-02-10 12:39:16 +0000

    fixed navbar issue so home is spaced away from logo

--- redshift/src/components/Navb.jsx
+++ redshift/src/components/Navb.jsx
@@ -1,21 +1,27 @@
-import {Link} from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import "../CSS/index.css";
 
 const Navb = () => {
-    return ( 
-        <div>
-            <Navbar variant="dark" bg="dark">
-              {/* <Container> */}
-                  <img src="../RedShift.png" alt="Redshift logo" height="80"></img>
-                <Navbar.Brand href="/home">Home</Navbar.Brand>
-                {/* <Navbar.Brand href="/lock">Lock</Navbar.Brand> */}
-                <Navbar.Brand href="/logout">Logout</Navbar.Brand>
-              {/* </Container> */}
-            </Navbar>
+  return (
+    <div>
+      <Navbar variant="dark" bg="dark">
+        {/* <Container> */}
+
+        <img src="../RedShift.png" alt="Redshift logo" height="80"></img>
+        <div className="home p-4">
+          <Navbar.Brand href="/home">Home</Navbar.Brand>
         </div>
-     );
-}
- 
+        {/* <Navbar.Brand href="/lock">Lock</Navbar.Brand> */}
+
+        <Navbar.Brand href="/logout"> Logout</Navbar.Brand>
+
+        {/* </Container> */}
+      </Navbar>
+    </div>
+  );
+};
+
 export default Navb;
